d7a91e881656016047051fce9adadd798df29740
Yezir971 <james_ahmedaly@yahoo.com>
2025-03-16 14:22:13 +0100

    fix : fix Bug default lang

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,11 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from '../data/texteApp';
+let langDefault = localStorage.getItem("lang") == "en" || localStorage.getItem("lang") == "fr" ? localStorage.getItem("lang") : "fr"
 i18n
   .use(initReactI18next)
   .init({
     resources: resources,
-    lng: "fr", // Langue par défaut
+    lng: langDefault, // Langue par défaut
     fallbackLng: "en",
     interpolation: { escapeValue: false },
   });
